92e5675312493bdc3d236e0c9c28267bcaf5e678
Cory Anderson <cory.anderson6@yahoo.com>
2023-04-08 15:47:52 -0500

    Added photos to gallery

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,15 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    
+{ id: 1, path: 'images/goat_small.jpg', title: 'Goat', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+
+{id: 2, path: 'Cart.jpg', title: 'Cart', description: 'Side view of a UTV.', likes: 0, },
+
+{id: 3, path: 'tiger.jpg', title: 'Tiger', description: 'Photo of Tiger', likes: 0 },
+
+{id: 4, path: 'train.jpg', title: 'Train', description: 'Photo of Train Arriving', likes: 0 },
+
+{id: 5, path: 'Van.jpg', title: 'Van', description: 'Photo of a Van on the beach.' , likes: 0 },
+
 ];
 
 module.exports = galleryItems;
