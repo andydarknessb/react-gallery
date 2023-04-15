29f707e1d96b891e4f850bf0acf085068afbd1d3
Cory Anderson <cory.anderson6@yahoo.com>
2023-04-15 09:57:21 -0500

    added fetch from server

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import GalleryList from './components/GalleryList/GalleryList';
+
+function App() {
+  const[galleryItems, setGalleryItems] = useState([]);
+}
+
+// Fetches gallery items from the server and stores them in state
+const fetchGalleryItems = async () => {
+  try {
+    const response = await axios.get('/gallery');
+    setGalleryItems(response.data);
+  } catch (error) {
+    console.error('Error fetching gallery items:', error);
+  }
+};
+
 
 function App() {
     return (
