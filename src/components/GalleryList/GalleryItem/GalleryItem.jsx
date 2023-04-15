41db88a24fb2fa383c4d7897095577525a951b90
Cory Anderson <cory.anderson6@yahoo.com>
2023-04-15 10:44:04 -0500

    added like count

--- src/components/GalleryList/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryList/GalleryItem/GalleryItem.jsx
@@ -9,5 +9,16 @@ function GalleryItem({ item }) {
         setShowDescription(!showDescription);
     };
 
+// Updated the like count for the item on the server
+const handleLike = async () => {
+    try {
+        await axios.put(`/gallery/like/${item.id}`);
+        window.location.reload(); // Refreshes the page to update the like count
+    } catch (error) {
+        console.error('Error updating like count:', error);
+    }
+};
 
+
+}
 }
