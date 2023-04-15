3c895f6c37be0d628b6c5ecfaa92c4004c39fba3
Cory Anderson <cory.anderson6@yahoo.com>
2023-04-15 11:28:48 -0500

    modified pictures

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -2,13 +2,13 @@ const galleryItems = [
     
 { id: 1, path: 'images/goat_small.jpg', title: 'Goat', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
 
-{id: 2, path: 'Cart.jpg', title: 'Cart', description: 'Side view of a UTV.', likes: 0, },
+{id: 2, path: 'images/Cart.jpeg', title: 'Cart', description: 'Side view of a UTV.', likes: 0, },
 
-{id: 3, path: 'tiger.jpg', title: 'Tiger', description: 'Photo of Tiger', likes: 0 },
+{id: 3, path: 'images/tiger.jpeg', title: 'Tiger', description: 'Photo of Tiger', likes: 0 },
 
-{id: 4, path: 'train.jpg', title: 'Train', description: 'Photo of Train Arriving', likes: 0 },
+{id: 4, path: 'images/train.jpeg', title: 'Train', description: 'Photo of Train Arriving', likes: 0 },
 
-{id: 5, path: 'Van.jpg', title: 'Van', description: 'Photo of a Van on the beach.' , likes: 0 },
+{id: 5, path: 'images/Van.jpeg', title: 'Van', description: 'Photo of a Van on the beach.' , likes: 0 },
 
 ];
 
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,36 +1,35 @@
-import React, {useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
-import GalleryList from './components/GalleryList/GalleryList';
+import GalleryList from '../GalleryList/GalleryList';
 
 function App() {
-  const[galleryItems, setGalleryItems] = useState([]);
+  const [galleryItems, setGalleryItems] = useState([]);
+
+  // Fetches gallery items from the server and stores them in state
+  const fetchGalleryItems = async () => {
+    try {
+      const response = await axios.get('/gallery');
+      setGalleryItems(response.data);
+    } catch (error) {
+      console.error('Error fetching gallery items:', error);
+    }
+  };
+
+  // Fetches gallery items on component mount
+  useEffect(() => {
+    fetchGalleryItems();
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Gallery of My Life</h1>
+      </header>
+      {/* Passes gallery items to the GalleryList component */}
+      <GalleryList galleryItems={galleryItems} />
+    </div>
+  );
 }
-
-// Fetches gallery items from the server and stores them in state
-const fetchGalleryItems = async () => {
-  try {
-    const response = await axios.get('/gallery');
-    setGalleryItems(response.data);
-  } catch (error) {
-    console.error('Error fetching gallery items:', error);
-  }
-};
-
-// Fetches gallery items on component mount
-useEffect(() => {
-  fetchGalleryItems();
-}, []);
-
-return (
-  <div className="App">
-    <header className="App-header">
-      <h1 className="App-title">Gallery of My Life</h1>
-    </header>
-    {/* Passes gallery items to the GalleryList component */}
-    <GalleryList galleryItems={galleryItems} />
-  </div>
-);
-
 
 export default App;
